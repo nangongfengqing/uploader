7c53198063174d821ebe2d79825520eb06bd87b6
liwq <liwq@nbrjwl.com>
2021-04-01 17:07:19 +0800

    uploader

--- src/uploader.ts
+++ src/uploader.ts
@@ -1,5 +1,28 @@
+interface Setting {
+  url: string
+  wrapper: HTMLElement
+  multiple: boolean
+  limit: number
+  autoUpload: boolean
+  accept: string
+  headers: object
+  data: object
+  withCredentials: boolean
+}
+
+/**
+ * base uploader definition
+ * @author liwq
+ */
 export default class Uploader {
-  setting: object
+  setting: Setting
+  uploadFiles: File[]
+  input: HTMLInputElement
+  changeHandler: Function
+  /**
+   * constructor
+   * @param option
+   */
   constructor(option = {}) {
     const defaultOption = {
       url: '',
@@ -8,6 +31,7 @@ export default class Uploader {
       limit: -1,
       autoUpload: true,
       accept: '*',
+
       headers: {},
       data: {},
       withCredentials: false,
@@ -16,5 +40,41 @@ export default class Uploader {
     this.init()
   }
 
-  private init() {}
+  private init() {
+    this.uploadFiles = []
+    this.input = this.initInputElement(this.setting)
+    this.changeHandler = (e: Event) => {
+      const files = (e.target as HTMLInputElement).files
+      const ret = this.callHook('choose', files)
+      // if (ret !== false) {
+      //   this.loadFiles(ret || files)
+      // }
+    }
+    // this.input.addEventListener('change', this.changeHandler)
+    this.setting.wrapper.appendChild(this.input)
+  }
+
+  private initInputElement(setting): HTMLInputElement {
+    const el = document.createElement('input')
+    Object.entries({
+      type: 'file',
+      accept: setting.accept,
+      multiple: setting.multiple,
+      hidden: true,
+    }).forEach(([key, value]) => {
+      el[key] = value
+    })
+    return el
+  }
+
+  on(evt: Event) {}
+  private callHook(evt: string, files: FileList) {}
+
+  chooseFile() {}
+  loadFiles(files: FileList) {}
+  removeFile(file: File) {}
+  clear() {}
+
+  upload(file: File) {}
+  private post(file: File) {}
 }
